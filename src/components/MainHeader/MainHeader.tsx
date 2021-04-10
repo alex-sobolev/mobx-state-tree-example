0c8768bb4439bc9b400991b5e2332910f5152b25
Alex Sobolev <alexandr.sobolev@deliveryhero.com>
2021-04-11 01:35:39 +0200

    Update MainHeader text

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -2,5 +2,5 @@ import React, { FC, useEffect } from 'react';
 import './MainHeader.css';
 
 export const MainHeader: FC = () => {
-  return <header className="MainHeader">An Example of Mobx with Classes for Base Store Components</header>;
+  return <header className="MainHeader">An example of Mobx with mobx-state-tree</header>;
 };
